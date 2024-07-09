5018e2120ebed8a5b94f7f4551936017a5f87b3d
litovn <niki.litovchenko@gmail.com>
2024-07-09 18:37:43 -0400

    update pipe manager

--- src/pipeline_manager.py
+++ src/pipeline_manager.py
@@ -1,12 +1,14 @@
 import os
 import yaml
+import subprocess
 import logging
-
+from dotenv import load_dotenv
 from kfp import dsl
 from kfp.kubernetes import mount_pvc, DeletePVC
 
 from kube.pvc_manager import *
 from kube.pipeline_run import *
+
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] - %(message)s', datefmt='%H:%M:%S')
 
@@ -20,7 +22,7 @@ def load_dag_configuration(dag_path):
     """
     with open(dag_path, 'r') as file:
         data = yaml.safe_load(file)
-        return data['System']['components'], data['System']['dependencies']
+        return data['System']['components'], data['System']['dependencies'], data['System']['input_media']
 
 
 # With download_from_pvc method defined in pvc_manager.py, it might be possible to search for the output file path
@@ -38,7 +40,7 @@ def get_output_filepath(output_dir: str):
 """
 
 
-def create_component(component_name: str):
+def create_component(username: str, component_name: str):
     """
     Creation of a reusable container component for the various pipeline steps
 
@@ -52,7 +54,7 @@ def create_component(component_name: str):
 @dsl.container_component
 def {comp_name}(input_path: str, output_path: str):
     return dsl.ContainerSpec(
-        image=f'myapp/{component_name}:latest',
+        image=f'{username}/{component_name}:latest',
         command=['python', 'main.py'],
         args=[
             '-i', input_path, '-o', output_path
@@ -77,39 +79,49 @@ def setup_component(component_name: str, input_path: str, output_dir: str, pvc_n
     comp_func = globals().get(component_name.replace('-', '_'))
     component_op = comp_func(input_path=input_path, output_path=output_dir)
     component_op = mount_pvc(component_op, pvc_name=pvc_name, mount_path='/mnt/data')
-    component_op.set_caching_options(False)
+    # Caching OFF specified in the pipeline run, but can be set to True if needed for a specific component
+    # component_op.set_caching_options(False)
 
     return component_op
 
 
-def generate_pipeline(dag_components: list, dag_dependencies: list, pvc_name: str, init_input_path: str):
+def generate_pipeline(username: str, dag_components: list, dag_dependencies: list, init_input: str):
     """
     Create the components function, then generate the pipeline dynamically based on the DAG configuration
 
+    :param dag_components: components defined in the DAG config
     :param dag_dependencies: dependencies defined in the DAG config
-    :param pvc_name: name of the pvc to use for the pipeline
-    :param init_input_path: path to the initial input file
+    :param init_input: name of the initial input file
     :return: pipeline function
     """
+
     for component in dag_components:
-        create_component(component)
+        create_component(username, component)
+    create_component(username, 'save-video')
 
     @dsl.pipeline(
         name="Generated Pipeline from YAML",
         description="Automatically generated pipeline based on application_dag.yaml"
     )
-    def dynamic_pipeline(video_path: str = init_input_path, pvc_name: str = pvc_name):
+    def dynamic_pipeline(pvc_name: str):
         base_mount = "/mnt/data"
         component_op = {}
         last_component = None
-        input_path = video_path
+        input_path = init_input
 
+        # Set up the save_video component as first component
+        output_dir = f"{base_mount}/"
+        component_op['save-video'] = setup_component('save-video', input_path, output_dir, pvc_name)
+
+        # Set up the other components based on dependencies
         for dependency in dag_dependencies:
             this_component, next_component, _ = dependency
 
             if this_component not in component_op:
                 output_dir_this = f"{base_mount}/{this_component}"
+                input_path = f"{base_mount}/{init_input}"
                 component_op[this_component] = setup_component(this_component, input_path, output_dir_this, pvc_name)
+                component_op[this_component].after(component_op['save-video'])
 
             if next_component not in component_op:
                 output_dir_next = f"{base_mount}/{next_component}"
@@ -124,17 +136,21 @@ def generate_pipeline(dag_components: list, dag_dependencies: list, pvc_name: st
 
 
 if __name__ == '__main__':
-    video_input_path = ' '
     dag_path = ' '
-    dag_components, dag_dependencies = load_dag_configuration(dag_path)
+
+    dag_components, dag_dependencies, media = load_dag_configuration(dag_path)
+
+    load_dotenv()
+    docker_username = os.getenv('DOCKER_USERNAME')
 
     pvc_name = create_pvc()
 
-    pipeline_func = generate_pipeline(dag_components=dag_components, dag_dependencies=dag_dependencies, pvc_name=pvc_name, init_input_path=video_input_path)
+    input_filename = os.path.basename(media)
+
+    pipeline_func = generate_pipeline(username=docker_username, dag_components=dag_components, dag_dependencies=dag_dependencies, init_input=input_filename)
     pipeline_filename = 'pipeline.yaml'
 
-    pipeline_run(video_input_path, pvc_name, pipeline_func, pipeline_filename)
+    pipeline_run(pvc_name, pipeline_func, pipeline_filename)
 
     local_path = ' '
-    # download_from_pvc(pvc_name, local_path)
-    # delete_pvc(pvc_name)
+    download_from_pvc(pvc_name, local_path)
